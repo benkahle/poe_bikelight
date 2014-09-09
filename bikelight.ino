633c2ed4c4df144867531cce2688f0af6c7ad2df
Ben Kahle <ben@kahle.org>
2014-09-09 13:45:25 -0400

    simplified bounce to use a function setting a single LED on and the rest off.

--- bikelight.ino
+++ bikelight.ino
@@ -17,28 +17,20 @@ void setup() {
   //Set up analog pin for reading
 }
 
+void oneOn(ledIndex) {
+  for (for i=0; i < sizeof(leds)/sizeof(int); i++) {
+    if (i === ledIndex) {
+      digitalWrite(leds[i], HIGH);
+    } else {
+      digitalWrite(leds[i], LOW);
+    }
+  }
+}
+
 void bounce(currentTime,lastButtonTime) {
   timeDiff = (currentTime - lastButtonTime) / 100;
   positionOn = floor(timeDiff) % 8;
-  if (positionOn == 0) {
-    digitalWrite(leds[0], HIGH);
-    digitalWrite(leds[1], LOW);
-  } else if (positionOn == 1) {
-    digitalWrite(leds[1], HIGH);
-    digitalWrite(leds[0], LOW);
-    digitalWrite(leds[2], LOW);
-  } else if (positionOn == 2) {
-    digitalWrite(leds[2], HIGH);
-    digitalWrite(leds[1], LOW);
-    digitalWrite(leds[3], LOW);
-  } else if (positionOn == 3) {
-    digitalWrite(leds[3], HIGH);
-    digitalWrite(leds[2], LOW);
-    digitalWrite(leds[4], LOW);
-  } else if (positionOn == 4) {
-    digitalWrite(leds[4], HIGH);
-    digitalWrite(leds[3], LOW);
-  }
+  oneOn(positionOn);
 }
 
 void setAllLeds(setPoint) {
@@ -85,15 +77,6 @@ void loop() {
     }
     sampleTime = currentTime + 100;
   }
-
-setPoint
-  //EXAMPLE
-  int pinNumber = 13;
-  digitalWrite(pinNumber, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
-  //END EXAMPLE
 
   //Read analog in, change counter as needed, choose which function to call
   //if Ain high, increment counter, dont read button again for ~100ms
